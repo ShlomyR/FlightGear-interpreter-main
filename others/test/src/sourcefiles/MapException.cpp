a38f123706b10bff412a8e31eea466a621c047ce
ShlomyR <rshlomy@gmail.com>
2023-05-03 14:55:19 +0300

    handle map exceptions

---
+++ others/test/src/sourcefiles/MapException.cpp
@@ -0,0 +1,21 @@
+#include "../hederfiles/MapException.hpp"
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <sstream>
+
+MapException::MapException(const std::string& message, const std::string& location)
+: m_message(message)
+, m_location(location)
+{}
+
+const char *MapException::what() const noexcept
+{
+    return m_message.c_str();
+}
+
+const char* MapException::where() const noexcept
+{
+    return m_location.c_str();
+}
