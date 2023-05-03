b13880c18f284c4180e923a9d2398394dc41c621
ShlomyR <rshlomy@gmail.com>
2023-05-03 14:54:09 +0300

    separate from parser

---
+++ others/test/src/sourcefiles/LinesPointer.cpp
@@ -0,0 +1,33 @@
+#include "../hederfiles/LinesPointer.hpp"
+
+size_t LinesPointer::m_pointerCounter;
+
+LinesPointer::LinesPointer()
+{
+    m_pointerCounter = 0;
+}
+
+void LinesPointer::incrementIndex()
+{
+    m_pointerCounter++;
+}
+
+void LinesPointer::setIndex(size_t i)
+{
+    m_pointerCounter = i;
+}
+
+void LinesPointer::addToIndex(size_t i)
+{
+    m_pointerCounter += i;
+}
+
+size_t LinesPointer::getIndex()
+{
+    return m_pointerCounter;
+}
+
+void LinesPointer::resetIndex()
+{
+    m_pointerCounter = 0;
+}
