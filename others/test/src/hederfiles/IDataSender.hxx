46fc839d76e50868a463b30ef94727bd1c247ca2
ShlomyR <rshlomy@gmail.com>
2023-05-03 15:52:35 +0300

    fixed untracked files

--- others/test/src/hederfiles/IDataSender.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class IDataSender {
-public:
-    virtual ~IDataSender() {}
-    virtual void sendData(std::string const& msg) = 0;
-};
--- others/test/src/sourcefiles/DataSender.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "../hederfiles/DataSender.hpp"
-
-void DataSender::sendData(std::string const& msg) {
-    std::cout << msg << "\n";
-}
