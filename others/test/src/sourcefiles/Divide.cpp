a894780f3c59f49f273e30d09902e02986b20288
ShlomyR <rshlomy@gmail.com>
2023-05-10 20:06:18 +0300

    organizing to folders

--- others/test/src/sourcefiles/Divide.cpp
+++
@@ -1,6 +0,0 @@
-#include "../hederfiles/Divide.hpp"
-
-double Divide::calc() const
-{
-    return m_right->calc() / m_left->calc();
-}
--- others/test/src/sourcefiles/IfBlockParser.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-#include "../hederfiles/IfBlockParser.hpp"
-
-void IfBlockParser::parse(std::vector<std::vector<std::string>> &lines)
-{
-    reserve_block_type(lines, "if");
-}
